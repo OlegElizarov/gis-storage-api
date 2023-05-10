656294362c208c26d840cb308269890bb1c1b5ee
oelizarov <oelizarov@ozon.ru>
2023-05-10 22:11:16 +0300

    Fix growth tree model

--- internal/models/growth_tree.go
+++ internal/models/growth_tree.go
@@ -6,12 +6,12 @@ import (
 )
 
 type GrowthTree struct {
-	GisID    int64    `db:"gis_id" json:"gis_id,omitempty" csv:"Номер дерева (ГИС/Прибит на ствол),omitempty"` // Номер дерева (ГИС/Прибит на ствол)
-	TS       DateTime `db:"ts" json:"ts,omitempty" csv:"ts,omitempty"`                                         // Координата Х
-	Age      int64    `db:"age" json:"age,omitempty" csv:"age,omitempty"`                                      // Координата Y
-	Diameter float64  `db:"diameter" json:"diameter,omitempty" csv:"diameter,omitempty"`                       // Высота ГИС (Митрофанов)
-	Height   float64  `db:"height" json:"height,omitempty" csv:"height,omitempty"`                             // Высота (Илья)
-	IsAlive  bool     `db:"is_alive" json:"is_alive,omitempty" csv:"is_alive,omitempty"`                       // №
+	GisID    int64    `db:"gis_id" json:"gis_id,omitempty" csv:"gis_id,omitempty"`       // Номер дерева (ГИС/Прибит на ствол)
+	TS       DateTime `db:"ts" json:"ts,omitempty" csv:"ts,omitempty"`                   // Координата Х
+	Age      int64    `db:"age" json:"age,omitempty" csv:"age,omitempty"`                // Координата Y
+	Diameter float64  `db:"diameter" json:"diameter,omitempty" csv:"diameter,omitempty"` // Высота ГИС (Митрофанов)
+	Height   float64  `db:"height" json:"height,omitempty" csv:"height,omitempty"`       // Высота (Илья)
+	IsAlive  bool     `db:"is_alive" json:"is_alive,omitempty" csv:"is_alive,omitempty"` // №
 }
 
 type DateTime struct {
